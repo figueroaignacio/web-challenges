423c4d1e2d73f608a027cc43fbfc306a1a6e94fe
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-12-06 23:59:47 -0300

    feat: add button styles

--- huddle-landing-page-with-alternating-feature-blocks/src/components/Button.tsx
+++ huddle-landing-page-with-alternating-feature-blocks/src/components/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
 
 export function Button({ children }: ButtonProps) {
   return (
-    <button className="bg-primary text-white px-12 py-3 rounded-full hover:brightness-125">
+    <button className="bg-primary text-white px-12 py-3 rounded-full hover:brightness-125 active:scale-95 transition-all duration-200 hover:scale-[1.03]">
       {children}
     </button>
   );
