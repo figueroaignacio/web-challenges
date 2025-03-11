27e4a73d36bac9a192cf4a1bb4db43c91df2d491
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-03-11 20:36:20 -0300

    make the header sticky

--- rest-countries-app/src/components/header.tsx
+++ rest-countries-app/src/components/header.tsx
@@ -3,7 +3,7 @@ import { ToggleTheme } from "./toggle-theme"
 
 export function Header() {
   return (
-    <header className="bg-card border-border border-b">
+    <header className="bg-card border-border border-b sticky top-0">
       <div className="max-w-6xl mx-auto flex items-center justify-between py-4 px-3">
         <Link to="/" className="font-bold text-2xl">Where in the world</Link>
         <ToggleTheme />
