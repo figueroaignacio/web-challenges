eae45e9adab1bc2e49dedbe6ef78c3b891bafe8e
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-03-29 20:45:19 -0300

    change file name

--- agency-landing-page/src/App.tsx
+++ agency-landing-page/src/App.tsx
@@ -1,7 +1,7 @@
 import { Header } from "./components/header";
 import { About } from "./sections/about";
-import { Services } from "./sections/features";
 import { Hero } from "./sections/hero";
+import { Services } from "./sections/services";
 
 export function App() {
   return (
